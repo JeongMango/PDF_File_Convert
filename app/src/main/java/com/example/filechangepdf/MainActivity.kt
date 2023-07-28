c5b16993b27f6b4f33dfe92e0d83eb203c29cee7
wjdaudrbs456@naver.com <war754123!>
2023-07-28 16:53:54 +0900

    [2023-07-28] PDF 버튼클릭시 패키지경로에 저장완료

--- app/src/main/java/com/example/filechangepdf/MainActivity.kt
+++ app/src/main/java/com/example/filechangepdf/MainActivity.kt
@@ -1,112 +1,90 @@
 package com.example.filechangepdf
 
-import android.Manifest
-import android.content.pm.PackageManager
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
 import android.graphics.Bitmap
 import android.graphics.Canvas
-import android.net.Uri
+import android.graphics.pdf.PdfDocument
 import android.os.Bundle
+import android.os.Environment
 import android.view.View
-import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
-import com.itextpdf.text.Document
-import com.itextpdf.text.Image
-import com.itextpdf.text.pdf.PdfDocument
-import com.itextpdf.text.pdf.PdfWriter
-import java.io.ByteArrayOutputStream
+import android.widget.Button
+import androidx.constraintlayout.widget.ConstraintLayout
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
-
-    private val PERMISSION_REQUEST_CODE = 123
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // 저장소 권한이 있는지 확인하고 없으면 권한 요청
-        if (!hasStoragePermission()) {
-            requestStoragePermission()
+        val btnClick = findViewById<Button>(R.id.btnClick)
+
+        btnClick.setOnClickListener {
+            val constraintLayout = findViewById<ConstraintLayout>(R.id.layout)
+//            val filename = "my_constraint_layout"
+//            saveConstraintLayoutAsPdf(this, constraintLayout, filename)
+            saveConstraintLayoutAsPdf(this, constraintLayout)
+        }
+    }
+    fun saveConstraintLayoutAsPdf(context: Context, constraintLayout: ConstraintLayout) {
+        val pdfDocument = PdfDocument()
+        val pageInfo = PdfDocument.PageInfo.Builder(constraintLayout.width, constraintLayout.height, 1).create()
+        val page = pdfDocument.startPage(pageInfo)
+        val canvas = page.canvas
+        val density = context.resources.displayMetrics.density
+        val scaledBitmap = Bitmap.createScaledBitmap(getBitmapFromView(constraintLayout), constraintLayout.width, constraintLayout.height, true)
+        canvas.drawBitmap(scaledBitmap, 0f, 0f, null)
+        pdfDocument.finishPage(page)
+
+        val currentTime = SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(Date())
+        val filename = "my_constraint_layout_$currentTime.pdf"
+
+        val file = File(getPdfDirectory(context), filename)
+        try {
+            val fileOutputStream = FileOutputStream(file)
+            pdfDocument.writeTo(fileOutputStream)
+            pdfDocument.close()
+        } catch (e: IOException) {
+            e.printStackTrace()
         }
     }
 
-    private fun hasStoragePermission(): Boolean {
-        return ContextCompat.checkSelfPermission(
-            this,
-            Manifest.permission.WRITE_EXTERNAL_STORAGE
-        ) == PackageManager.PERMISSION_GRANTED
-    }
+    /*fun saveConstraintLayoutAsPdf(context: Context, constraintLayout: ConstraintLayout, filename: String) {
+        val pdfDocument = PdfDocument()
+        val pageInfo = PdfDocument.PageInfo.Builder(constraintLayout.width, constraintLayout.height, 1).create()
+        val page = pdfDocument.startPage(pageInfo)
+        val canvas = page.canvas
+        val density = context.resources.displayMetrics.density
+        val scaledBitmap = Bitmap.createScaledBitmap(getBitmapFromView(constraintLayout), constraintLayout.width, constraintLayout.height, true)
+        canvas.drawBitmap(scaledBitmap, 0f, 0f, null)
+        pdfDocument.finishPage(page)
 
-    private fun requestStoragePermission() {
-        ActivityCompat.requestPermissions(
-            this,
-            arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
-            PERMISSION_REQUEST_CODE
-        )
-    }
-
-    fun createPdf(view: View) {
-        if (hasStoragePermission()) {
-            // PDF로 변환할 레이아웃의 부모 레이아웃을 찾기
-            val viewToConvert = findViewById<View>(R.id.parentLayout)
-            // 레이아웃을 비트맵으로 변환
-            val bitmap = viewToBitmap(viewToConvert)
-            // 비트맵으로부터 PDF 생성
-            createPdfFromBitmap(bitmap)
-        } else {
-            Toast.makeText(
-                this,
-                "외부 저장소에 쓰기 권한이 필요합니다.",
-                Toast.LENGTH_SHORT
-            ).show()
+        val file = File(getPdfDirectory(context), "$filename.pdf")
+        try {
+            val fileOutputStream = FileOutputStream(file)
+            pdfDocument.writeTo(fileOutputStream)
+            pdfDocument.close()
+        } catch (e: IOException) {
+            e.printStackTrace()
         }
-    }
+    }*/
 
-    private fun viewToBitmap(view: View): Bitmap {
-        // 뷰의 너비와 높이를 측정
-        view.measure(View.MeasureSpec.UNSPECIFIED, View.MeasureSpec.UNSPECIFIED)
-        // 뷰와 같은 크기의 비트맵 생성
-        val bitmap = Bitmap.createBitmap(
-            view.measuredWidth,
-            view.measuredHeight,
-            Bitmap.Config.ARGB_8888
-        )
-        // 뷰를 비트맵에 그림
+    private fun getBitmapFromView(view: View): Bitmap {
+        val bitmap = Bitmap.createBitmap(view.width, view.height, Bitmap.Config.ARGB_8888)
         val canvas = Canvas(bitmap)
-        view.layout(0, 0, view.measuredWidth, view.measuredHeight)
         view.draw(canvas)
         return bitmap
     }
 
-    private fun createPdfFromBitmap(bitmap: Bitmap) {
-        // 외부 저장소에 파일 저장 경로 설정
-        val pdfFilePath = "${externalCacheDir?.absolutePath}/output.pdf"
-        val uri = Uri.parse("file://$pdfFilePath")
-
-        contentResolver.openOutputStream(uri)?.let { outputStream ->
-            val pdfDocument = PdfDocument(PdfWriter(outputStream))
-            val document = Document(pdfDocument)
-
-            try {
-                val image = Image.getInstance(bitmapToByteArray(bitmap))
-
-                pdfDocument.addNewPage()
-                document.add(image)
-
-            } catch (e: Exception) {
-                e.printStackTrace()
-            } finally {
-                document.close()
-                pdfDocument.close()
-                outputStream.close()
-            }
+    private fun getPdfDirectory(context: Context): File {
+        val directory = File(context.getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS), "PDFs")
+        if (!directory.exists()) {
+            directory.mkdirs()
         }
-    }
-
-    private fun bitmapToByteArray(bitmap: Bitmap): ByteArray {
-        val stream = ByteArrayOutputStream()
-        bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream)
-        return stream.toByteArray()
+        return directory
     }
 }
